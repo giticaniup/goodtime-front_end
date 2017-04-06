99ea0c84b43d7af2373aec768f282114566ca9af
钟乘永 <zhongcy93@gmail.com>
2017-04-06 23:08:58 +0800

    编译代码

---
+++ src/components/Chat.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <mt-button type="primary">中文</mt-button>
+    <mt-button type="primary">primary1</mt-button>
+  </div>
+</template>
+
+<script>
+import {Toast, Button} from 'mint-ui'
+export default {
+  name: 'chat',
+  data () {
+    return {
+      title: '聊天程序'
+    }
+  },
+  components: {
+    Toast: Toast,
+    mtButton: Button
+  }
+}
+Toast('测试')
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Chat from '@/components/Chat'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat
+    },
     {
       path: '/',
       name: 'Hello',
